fe711b77769794e7d73a9e402678c3627d94d2f0
Krisque <chrisemke@protonmail.com>
2025-06-22 21:50:36 -0300

    style(emacs-yaml-mode): change indentation

--- .config/emacs/config/packages.el
+++ .config/emacs/config/packages.el
@@ -12,11 +12,12 @@
 
 (use-package yaml-mode
 	:ensure t
-	:hook ((yaml-mode yaml-ts-mode) . (lambda ()
-															(setq-local indent-tabs-mode nil)
-															(setq-local yaml-indent-offset 2)
-														)
-			)
+	:hook
+	((yaml-mode yaml-ts-mode) . (lambda ()
+																(setq-local indent-tabs-mode nil)
+																(setq-local yaml-indent-offset 2)
+															)
+	)
 )
 
 (provide 'packages)
