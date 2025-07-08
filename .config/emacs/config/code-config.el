1d698a025449a7fc55b37df7b9383051479cc018
Krisque <chrisemke@protonmail.com>
2025-07-08 00:26:12 -0300

    feat(emacs-code): add visual elements for completion and beautify error trace

--- .config/emacs/config/code-config.el
+++ .config/emacs/config/code-config.el
@@ -6,18 +6,35 @@
 	:ensure t
 )
 
+(use-package lsp-ui
+	:commands lsp-ui-mode
+	:ensure t
+)
+
+(use-package lsp-treemacs
+	:commands lsp-treemacs-errors-list
+	:config (lsp-treemacs-sync-mode t)
+	:ensure t
+)
+
 (use-package flycheck
 	:ensure t
 	:init (global-flycheck-mode)
 )
 
-(use-package prog-mode
+(use-package company
+	:config (global-company-mode t)
 	:custom
-	(which-func-display 'header)
+	(company-idle-delay 0)
+	(company-minimum-prefix-length 1)
+	:ensure t
+)
+
+(use-package prog-mode
+	:custom (which-func-display 'header)
 	:ensure nil
 	:hook ((prog-mode . which-function-mode))
-	:init
-	(setq which-func-unknown "")
+	:init (setq which-func-unknown "")
 )
 
 (require 'python-config)
