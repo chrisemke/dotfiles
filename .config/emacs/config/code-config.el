b5c2bbba6096cf1a32eac2441fbec59ee52043a6
Krisque <chrisemke@protonmail.com>
2025-07-29 20:55:42 -0300

    fix(emacs-code): add lsp-ivy, dap-mode, dockerfile-mode and lsp-diagnostics, also move projectile and yaml out of packages-config

--- .config/emacs/config/code-config.el
+++ .config/emacs/config/code-config.el
@@ -1,9 +1,16 @@
 (use-package lsp-mode
+	:bind ("C-." . lsp-execute-code-action)
 	:commands (lsp lsp-deferred)
 	:custom
 	(lsp-keymap-prefix "C-c l")
 	(lsp-auto-configure t)
 	:ensure t
+	:hook (lsp-mode . lsp-enable-which-key-integration)
+)
+
+(use-package lsp-diagnostics
+	:after flycheck
+	:config (lsp-diagnostics-flycheck-enable)
 )
 
 (use-package lsp-ui
@@ -15,6 +22,17 @@
 	:commands lsp-treemacs-errors-list
 	:config (lsp-treemacs-sync-mode t)
 	:ensure t
+	:init (setq lsp-treemacs-theme "nerd-icons")
+)
+
+(use-package lsp-ivy
+	:commands lsp-ivy-workspace-symbol
+	:ensure t
+)
+
+(use-package dap-mode
+	:after lsp-mode
+	:ensure t
 )
 
 (use-package flycheck
@@ -23,6 +41,7 @@
 )
 
 (use-package company
+	:bind ("C-SPC" . company-complete)
 	:config (global-company-mode t)
 	:custom
 	(company-idle-delay 0)
@@ -30,13 +49,30 @@
 	:ensure t
 )
 
-(use-package prog-mode
-	:custom (which-func-display 'header)
-	:ensure nil
-	:hook ((prog-mode . which-function-mode))
-	:init (setq which-func-unknown "")
+(use-package dockerfile-mode
+	:defer t
+	:ensure t
 )
 
+(use-package projectile
+	:config
+	(projectile-mode t)
+	(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+	:custom (projectile-project-search-path '("~/"))
+	:ensure t
+)
+
+(use-package yaml-mode
+	:defer t
+	:ensure t
+	:hook ((yaml-mode yaml-ts-mode) . (lambda ()
+																			(setq-local indent-tabs-mode nil)
+																			(setq-local yaml-indent-offset 2)
+																		)
+				)
+)
+
+(require 'guile-config)
 (require 'python-config)
 
 (provide 'code-config)
--- .config/emacs/config/packages-config.el
+++ .config/emacs/config/packages-config.el
@@ -2,22 +2,4 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-(use-package projectile
-	:config
-	(projectile-mode t)
-	:custom
-	(projectile-project-search-path '("~/"))
-	:ensure t
-)
-
-(use-package yaml-mode
-	:ensure t
-	:hook
-	((yaml-mode yaml-ts-mode) . (lambda ()
-																(setq-local indent-tabs-mode nil)
-																(setq-local yaml-indent-offset 2)
-															)
-	)
-)
-
 (provide 'packages-config)
