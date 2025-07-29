6fce06286865508cc0d46abb264d5732185eb580
Krisque <chrisemke@protonmail.com>
2025-07-29 20:56:14 -0300

    feat(emacs-keys): add select all C-a and comment C-/

--- .config/emacs/config/keys-config.el
+++ .config/emacs/config/keys-config.el
@@ -1,17 +1,17 @@
 (cua-mode t)
 
 (use-package dtrt-indent
-	:custom
-	(dtrt-indent-run-after-smie t)
-	:config
-	(dtrt-indent-global-mode)
+	:custom (dtrt-indent-run-after-smie t)
+	:config (dtrt-indent-global-mode)
 	:ensure t
-	:hook
-	(prog-mode . dtrt-indent-mode)
+	:hook (prog-mode . dtrt-indent-mode)
 )
 
 (setq-default indent-tabs-mode t
 	standard-indent 2
 )
+
+(global-set-key (kbd "C-a") 'mark-whole-buffer)
+(global-set-key (kbd "C-/") 'comment-dwim)
 
 (provide 'keys-config)
