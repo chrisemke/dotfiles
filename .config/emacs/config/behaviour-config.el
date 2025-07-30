10116a7bf7684af42091cbfadf57acb5f3686e82
Krisque <chrisemke@protonmail.com>
2025-07-29 21:02:09 -0300

    feat(emacs-behaviour): add ivy, swiper, counsel and which-key

--- .config/emacs/config/behaviour-config.el
+++ .config/emacs/config/behaviour-config.el
@@ -4,14 +4,30 @@
 
 (editorconfig-mode t)
 
-(use-package treesit-auto
-	:config
-	(global-treesit-auto-mode)
-	:custom
-	(treesit-auto-install t)
+(use-package ivy
+	:config (ivy-mode)
 	:ensure t
 )
 
+(use-package swiper
+	:bind ("C-f" . swiper)
+	:ensure t
+)
+
+(use-package counsel
+	:config (counsel-mode)
+	:ensure t
+)
+
+(use-package treesit-auto
+	:config (global-treesit-auto-mode)
+	:custom (treesit-auto-install t)
+	:ensure t
+)
+
+(use-package which-key
+	:config (which-key-mode)
+)
 
 (fset 'yes-or-no-p 'y-or-n-p)
 
