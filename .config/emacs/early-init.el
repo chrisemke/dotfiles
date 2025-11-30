a3fed1af44d51dd85c211a988dbf254f1755b32e
Krisque <chrisemke@protonmail.com>
2025-11-30 00:57:28 -0300

    feat(emacs-title-bar): set title bar to be transparent

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -3,4 +3,8 @@
 (menu-bar-mode 0)
 (tool-bar-mode 0)
 
+;; Transparent title bar
+(set-frame-parameter nil 'ns-transparent-titlebar t)
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+
 (provide 'early-init)
