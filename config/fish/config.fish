281832b2d8fd2976e9cfc24b37ad0652adc865c0
Krisque <chrisemke@protonmail.com>
2026-01-06 17:29:36 -0300

    refactor(fish): tab indented, remove greetings and fix emacs --help

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,21 +1,14 @@
-if status is-interactive
-    starship init fish | source
+starship init fish | source
 
-    function fish_greeting
-        echo "     (o_"
-        echo "(o_  //\\"
-        echo "(/)_ V_/_"
-    end
-
-end
+set -g fish_greeting
 
 function emacs
-    set emacspath (which emacs)
-    if contains -- --no-window-system $argv; or contains -- -nw $argv
-        $emacspath $argv
-    else
-        $emacspath $argv &
-    end
+	set emacspath (which emacs)
+	if contains -- --help $argv; or contains -- --no-window-system $argv; or contains -- -nw $argv
+		$emacspath $argv
+	else
+		$emacspath $argv &
+	end
 end
 
 alias git-tree='git log --oneline --graph --decorate --all'
