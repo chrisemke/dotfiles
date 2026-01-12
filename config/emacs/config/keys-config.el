bd5873d038f760b64ca6468475577ad4865bf438
Krisque <chrisemke@protonmail.com>
2026-01-11 23:18:16 -0300

    feat(emacs-cursor): add multiple cursors

--- config/emacs/config/keys-config.el
+++ config/emacs/config/keys-config.el
@@ -22,6 +22,15 @@
 	:ensure t
 	)
 
+(use-package multiple-cursors
+	:bind (("C->" . mc/mark-next-like-this)
+				 ("C-<" . mc/skip-to-next-like-this)
+				 ("C-M->" . mc/mark-all-like-this)
+				 ("C-{" . mc/mark-previous-like-this)
+				 ("C-}" . mc/skip-to-previous-like-this))
+	:ensure t
+	)
+
 (global-set-key (kbd "C-a") 'mark-whole-buffer)
 
 (provide 'keys-config)
