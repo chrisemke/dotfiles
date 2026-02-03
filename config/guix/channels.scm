722fd948426774b4e760c0211570266a32b57262
Krisque <chrisemke@protonmail.com>
2026-02-03 17:59:38 -0300

    fix(guix-channels): re-do file to don't be a symlink

--- config/guix/channels.scm
+++ config/guix/channels.scm
@@ -1,1 +1,37 @@
-/gnu/store/ml8vw59x6fpch4ipc8fkmdfzai87yws7-channels.scm
+(list
+ (channel
+	(name 'guix)
+	(url "https://git.guix.gnu.org/guix.git")
+	(branch "master")
+	(introduction
+	 (make-channel-introduction
+		"9edb3f66fd807b096b48283debdcddccfea34bad"
+		(openpgp-fingerprint
+		 "BBB0 2DDF 2CEA F6A8 0D1D	 E643 A2A0 6DF2 A33A 54FA"))))
+ (channel
+	(name 'nonguix)
+	(url "https://gitlab.com/nonguix/nonguix")
+	(branch "master")
+	(introduction
+	 (make-channel-introduction
+		"897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+		(openpgp-fingerprint
+		 "2A39 3FFF 68F4 EF7A 3D29	 12AF 6F51 20A0 22FB B2D5"))))
+ (channel
+	(name 'saayix)
+	(url "https://codeberg.org/look/saayix")
+	(branch "main")
+	(introduction
+	 (make-channel-introduction
+		"12540f593092e9a177eb8a974a57bb4892327752"
+		(openpgp-fingerprint
+		 "3FFA 7335 973E 0A49 47FC	 0A8C 38D5 96BE 07D3 34AB"))))
+ (channel
+	(name 'radix)
+	(url "https://codeberg.org/anemofilia/radix.git")
+	(branch "main")
+	(introduction
+	 (make-channel-introduction
+		"f9130e11e35d2c147c6764ef85542dc58dc09c4f"
+		(openpgp-fingerprint
+		 "F164 709E 5FC7 B32B AEC7	 9F37 1F2E 76AC E3F5 31C8")))))
