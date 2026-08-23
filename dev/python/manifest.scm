47b4f10890e0a429531497a818e9e2b8524c0cd4
Krisque <chrisemke@protonmail.com>
2026-08-23 02:14:25 -0300

    guix python-dev: add python manifest and shell binary entrypoint

---
+++ dev/python/manifest.scm
@@ -0,0 +1,28 @@
+(define-module (dev python-dev)
+  #:use-module ((guix profiles)										#:select (packages->manifest))
+  #:use-module ((gnu packages base)								#:select (coreutils grep which))
+  #:use-module ((gnu packages bash)								#:select (bash))
+  #:use-module ((gnu packages commencement)				#:select (gcc-toolchain))
+  #:use-module ((gnu packages compression)				#:select (zlib))
+  #:use-module ((gnu packages curl)								#:select (curl))
+  #:use-module ((gnu packages nss)								#:select (nss-certs))
+  #:use-module ((gnu packages package-management) #:select (pipx))
+  #:use-module ((gnu packages pkg-config)					#:select (pkg-config))
+  #:use-module ((gnu packages python-xyz)					#:select (python-rassumfrassum))
+  #:use-module ((gnu packages version-control)		#:select (git))
+  )
+
+(packages->manifest
+ (list
+  ;; base
+  bash coreutils grep which
+  ;; network and TLS: the rustup installer, cargo, git
+  curl git nss-certs
+  ;; linking: rustup ships no linker, and every *-sys crate needs one
+  gcc-toolchain pkg-config zlib
+  ;; Python software to install libs globally (global virtualenv)
+  pipx
+  ;; Connect an LSP client to multiple LSP servers.
+  python-rassumfrassum
+  ))
+
