d73dc59652a22de08b96f07da7421c61e75bd445
Krisque <chrisemke@protonmail.com>
2026-09-10 01:49:10 -0300

    guix channels: update channels in use

--- config/guix/channels.scm
+++ config/guix/channels.scm
@@ -1,41 +1,30 @@
-(list
- (channel
-	(name 'guix)
-	(url "https://git.guix.gnu.org/guix.git")
-	(branch "master")
-	(commit "c3a4982ca9a161b39af76fad1b5bdaf6050635e7")
-	(introduction
-	 (make-channel-introduction
-		"9edb3f66fd807b096b48283debdcddccfea34bad"
-		(openpgp-fingerprint
-		 "BBB0 2DDF 2CEA F6A8 0D1D	 E643 A2A0 6DF2 A33A 54FA"))))
- (channel
-	(name 'nonguix)
-	(url "https://gitlab.com/nonguix/nonguix")
-	(branch "master")
-	(commit "bf39542ca537fde8839b209ac21d6f3254469b15")
-	(introduction
-	 (make-channel-introduction
-		"897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-		(openpgp-fingerprint
-		 "2A39 3FFF 68F4 EF7A 3D29	 12AF 6F51 20A0 22FB B2D5"))))
- (channel
-	(name 'saayix)
-	(url "https://codeberg.org/look/saayix")
-	(branch "main")
-	(commit "c72163c06fef42607f7be6782bb4e627c542bce0")
-	(introduction
-	 (make-channel-introduction
-		"12540f593092e9a177eb8a974a57bb4892327752"
-		(openpgp-fingerprint
-		 "3FFA 7335 973E 0A49 47FC	 0A8C 38D5 96BE 07D3 34AB"))))
- (channel
-	(name 'radix)
-	(url "https://codeberg.org/anemofilia/radix.git")
-	(branch "main")
-	(commit "0669b69d8cdfd7cc3ceda31fee1de603adf2d30d")
-	(introduction
-	 (make-channel-introduction
-		"f9130e11e35d2c147c6764ef85542dc58dc09c4f"
-		(openpgp-fingerprint
-		 "F164 709E 5FC7 B32B AEC7	 9F37 1F2E 76AC E3F5 31C8")))))
+(list (channel
+			 (name 'guix)
+			 (url "https://git.guix.gnu.org/guix.git")
+			 (branch "master")
+			 (commit "d21edc51a4e41015eeefef532566c99060dd0e16")
+			 (introduction
+				(make-channel-introduction
+				 "9edb3f66fd807b096b48283debdcddccfea34bad"
+				 (openpgp-fingerprint
+					"BBB0 2DDF 2CEA F6A8 0D1D	 E643 A2A0 6DF2 A33A 54FA"))))
+			(channel
+			 (name 'nonguix)
+			 (url "https://gitlab.com/nonguix/nonguix.git")
+			 (branch "master")
+			 (commit "accdba77be815bbe00868145dd66d68056eecf7a")
+			 (introduction
+				(make-channel-introduction
+				 "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+				 (openpgp-fingerprint
+					"2A39 3FFF 68F4 EF7A 3D29	 12AF 6F51 20A0 22FB B2D5"))))
+			(channel
+			 (name 'saayix)
+			 (url "https://codeberg.org/look/saayix.git")
+			 (branch "main")
+			 (commit "9d22922ef477f82b442d533c376baac4fe666268")
+			 (introduction
+				(make-channel-introduction
+				 "12540f593092e9a177eb8a974a57bb4892327752"
+				 (openpgp-fingerprint
+					"3FFA 7335 973E 0A49 47FC	 0A8C 38D5 96BE 07D3 34AB")))))
