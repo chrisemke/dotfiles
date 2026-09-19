1df7d2999ef71ce18e652266163c1f78d786dd75
Krisque <chrisemke@protonmail.com>
2026-09-18 21:38:26 -0300

    guix home: ajust env vars to have XDG_BIN_HOME

--- config/guix/home.scm
+++ config/guix/home.scm
@@ -166,8 +166,11 @@
 		 (simple-service
 			'home-shell-environment-variables
 			home-environment-variables-service-type
-			`(("PATH" . "$HOME/.local/bin:$PATH")
-				("XDG_DATA_DIRS" . "/var/lib/flatpak/exports/share:$XDG_DATA_HOME/flatpak/exports/share:$XDG_DATA_DIRS")
+			`(("XDG_DATA_DIRS" . "/var/lib/flatpak/exports/share:\
+														$XDG_DATA_HOME/flatpak/exports/share:\
+														$XDG_DATA_DIRS")
+				("XDG_BIN_HOME" . "$HOME/.local/bin")
+				("PATH" . "$XDG_BIN_HOME:$PATH")
 				("XDG_PROJECTS_DIR" . "$HOME/Projects")
 				("LESSOPEN" . "| src-hilite-lesspipe.sh %s")
 				("LESS" . " --raw-control-chars --tabs=2 --LINE-NUMBERS ")))))))
